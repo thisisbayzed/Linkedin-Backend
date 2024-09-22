f3c4a825c25b9eb72fefdfc5e554c31c7ad8aa3b
Bayzed Bostami <bayzed.dev@gmail.com>
2024-09-22 21:53:27 +0600

    build a indivual user notifications v1

--- src/controllers/notification.ts
+++ src/controllers/notification.ts
@@ -1,1 +1,30 @@
 import { NextFunction, Request, Response } from "express";
+import Notification from "../models/notification.model";
+
+const userNotification = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = req.user._id;
+
+    const notifications = await Notification.find({ recipient: userId })
+      .sort({
+        createdAt: -1,
+      })
+      .populate("sender", "name username profilePic")
+      .populate("post")
+      .populate("comment");
+
+    if (notifications.length === 0) {
+      return res.status(404).json({ message: "Notifications not found" });
+    }
+
+    res.status(200).json({ notifications });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export { userNotification };
--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -1,5 +1,8 @@
 import express from "express";
-
+import authHandler from "../middlewares/authHandler";
+import { userNotification } from "../controllers/notification";
 const NotificationRouter = express.Router();
 
-export default NotificationRouter
+NotificationRouter.get("/", authHandler, userNotification);
+
+export default NotificationRouter;
