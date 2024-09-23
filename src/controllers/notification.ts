9e7b09cc1d859337f8c42f435e9a330c8b0a2ca9
Bayzed Bostami <bayzed.dev@gmail.com>
2024-09-23 19:16:13 +0600

    add notification api v1

--- src/controllers/notification.ts
+++ src/controllers/notification.ts
@@ -1,1 +1,84 @@
 import { NextFunction, Request, Response } from "express";
+import Notification from "../models/notification.model";
+
+// get the notification
+const userNotification = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = req.user._id;
+
+    const notifications = await Notification.find({ recipient: userId })
+      .sort({
+        createdAt: -1,
+      })
+      .populate("sender", "name username profilePic")
+      .populate("post")
+      .populate("comment");
+
+    if (notifications.length === 0) {
+      return res.status(404).json({ message: "Notifications not found" });
+    }
+
+    res.status(200).json({ notifications });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// mark notification as read
+const markNotificationAsRead = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const notificationId = req.params.id;
+
+    const notification = await Notification.findByIdAndUpdate(
+      {_id:notificationId , recipient:req.user._id},
+      { isRead: true },
+      { new: true }
+    );
+
+    if (!notification) {
+      return res.status(404).json({ message: "Notification not found" });
+    }
+
+    res.status(200).json({ notification });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// delete notification
+const deleteNotification = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const notificationId = req.params.id;
+
+    const notification = await Notification.findByIdAndDelete({
+      _id: notificationId,
+      recipient: req.user._id,
+    });
+
+    if (!notification) {
+      return res.status(404).json({ message: "Notification not found" });
+    }
+
+    res.status(200).json({ message: "Notification deleted successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+
+
+export { userNotification , markNotificationAsRead , deleteNotification };
+
--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import authHandler from "../middlewares/authHandler";
-<<<<<<< HEAD
 import { deleteNotification, markNotificationAsRead, userNotification } from "../controllers/notification";
 const NotificationRouter = express.Router();
 
@@ -8,12 +7,5 @@ NotificationRouter.get("/", authHandler, userNotification);
 NotificationRouter.put("/:id", authHandler , markNotificationAsRead)
 NotificationRouter.delete("/:id", authHandler , deleteNotification)
 
-export default NotificationRouter;
-=======
-import { userNotification } from "../controllers/notification";
-const NotificationRouter = express.Router();
+export default NotificationRouter
 
-NotificationRouter.get("/", authHandler, userNotification);
-
-export default NotificationRouter;
->>>>>>> f3c4a825c25b9eb72fefdfc5e554c31c7ad8aa3b
