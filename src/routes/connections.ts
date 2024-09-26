455523d5ae19c8b56559a73def7474f507ae691a
Bayzed Bostami <bayzed.dev@gmail.com>
2024-09-26 23:17:42 +0600

    completed the full-stack backend

--- src/routes/connections.ts
+++ src/routes/connections.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import authHandler from "../middlewares/authHandler";
-import { acceptRequest, friendRequest, getFriendlist, getFriendRequests, rejectRequest } from "../controllers/connections";
+import { acceptRequest, friendRequest, getFriendlist, getFriendRequests, rejectRequest, removeFriend } from "../controllers/connections";
 const ConnectionRouter = express.Router();
 
 ConnectionRouter.post("/request/:friendRequestId", authHandler, friendRequest);
@@ -8,5 +8,6 @@ ConnectionRouter.put("/accepted/:connectionId", authHandler, acceptRequest);
 ConnectionRouter.put("/rejected/:connectionId", authHandler, rejectRequest);
 ConnectionRouter.get("/friendRequests", authHandler , getFriendRequests);
 ConnectionRouter.get("/friendlist", authHandler , getFriendlist);
+ConnectionRouter.delete("/:friendId", authHandler , removeFriend);
 
 export default ConnectionRouter;
